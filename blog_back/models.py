29678bcdc9cb175ed9c57bd053de3339ce3bf3b7
Rafael Fernandes <rafaellemosfernandes@gmail.com>
2022-07-23 16:45:26 -0300

    Projeto Iniciado

---
+++ blog_back/models.py
@@ -0,0 +1,45 @@
+from tkinter import ACTIVE
+from turtle import title
+from unicodedata import category
+from django.db import models
+
+class Category(models.Model):
+    title = models.CharField(max_length=255)
+    slug = models.SlugField()
+
+    class Meta:
+        ordering = ('title',)
+
+    def __str__(self):
+        return self.title
+
+    def get_absolute_url(self):
+        return '/%s/' % self.slug
+
+class Post(models.Model):
+    ACTIVE = 'active'
+    DRAFT = 'draft'
+
+    CHOICES_STATUS =  (
+        (ACTIVE, 'Active'),
+        (DRAFT, 'Draft')
+    )
+
+    category = models.ForeignKey(Category, related_name='posts', on_delete=models.CASCADE)
+    title = models.CharField(max_length=255)
+    slug = models.SlugField()
+    intro = models.TextField(max_length=255)
+    body = models.TextField()
+    created_at = models.DateTimeField(auto_now_add=True)
+    status = models.CharField(max_length=10, choices=CHOICES_STATUS, default=ACTIVE)
+    image = models.ImageField(upload_to='uploads/', blank=True, null=True)
+    
+
+    class Meta:
+        ordering = ('-created_at',)
+
+    def __str__(self):
+        return self.title
+
+    def get_absolute_url(self):
+        return '/%s/%s/' % (self.category.slug, self.slug)
